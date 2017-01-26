c286679efe82817272a849f2581280c2c0ef2388
Jenny Lai <jenny.lai@flatironschool.com>
2017-01-25 20:52:21 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
+var totalBatteries = batteryBatches.reduce(function(x, y){
+  return x + y
+}, 0)
+
+
 const monologueLines = [
   'Who are you talking to right now?',
   'Who is it you think you see?',
@@ -16,3 +21,18 @@ const monologueLines = [
   'No.',
   'I am the one who knocks!'
 ];
+
+//{ '7': 2, '9': 1 }
+// array.reduce(function(accumulator, currentValue, currentIndex, array) {
+//   return accumulator + currentValue;
+// });
+
+var wordCountMap = monologueLines.reduce(function(wordCount, monologueLine) {
+  var wordLength = monologueLine.split(' ').length
+  if (wordLength in wordCount) {
+    wordCount[wordLength] ++
+  } else {
+    wordCount[wordLength] = 1
+  }
+  return wordCount
+}, {})
